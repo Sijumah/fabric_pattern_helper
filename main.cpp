2626a59b58eee03b47294a17c6749ecd05398004
Sijumah Cartwright <sijumah.cartwright.20@cnu.edu>
2021-10-31 22:18:27 -0400

    progress for working selwhere

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 
 #include <optional>
 #include <math.h>
+
+
 struct point{
 
 double x=0;
@@ -8,6 +10,12 @@ double y=0;
 
 };
 
+double pythagorean_theorum(const std::optional<double>& a={}, const std::optional<double>& b={}, const std::optional<double>& c={}){
+    if (!a.has_value()){return sqrt(pow(*c,2)-pow(*b,2));}  //solve for a
+    else if (!b.has_value()){return sqrt(pow(*c,2)-pow(*a,2));}  //solve for b
+    else if (!c.has_value()){return sqrt(pow(*a,2)+pow(*b,2));}  //solve for c
+    else return 0;
+};
 
 //rad to degrees: num* (180/pi) 
 //angle a: (leg of triangle as opposed to base): arcsin(a/c).  
@@ -15,10 +23,10 @@ double y=0;
 struct sewtriangle{
 
     bool qshowtriangle;
-    point p1;
-    point p2;
-    point tripoint;
-    double tribase;
+    point p1;  //leg
+    point p2;  //base
+    point tripoint;  //middle, right angle point
+    double tribase;  //lengths
     double trileg;
     double trihypot;
     double throwangle;  //angle opposite a, the leg
@@ -29,34 +37,71 @@ struct sewtriangle{
     point calc_tripoint(){return point{this->p2.x-this->p1.x,this->p2.y-this->p1.y};};
 
     double calc_throwangle(){return asin(this->tribase/this->trileg)*(180/3.141596);};  //in degrees
-    double calc_lesserangle(){return asin(this->tribase/this->trihypot)*(180/3.141596);};
+    double calc_lesserangle(){return asin(this->tribase/this->trihypot)*(180/3.141596);};  //in degrees
     double calc_tribase(){return this->p2.x-this->p1.x;}
     double calc_trileg(){return this->p2.y-this->p1.y;}
     double calc_hypot(){return pow(pow(this->tribase,2.0)+pow(this->trileg,2.0),0.5);}
-    void applyangles(){this->throwangle=this->calc_throwangle(); this->lesserangle=this->calc_lesserangle();};
+
+    void recalculate_from_points(){
+        this->tripoint=calc_tripoint();
+        this->trileg=calc_trileg();
+        this->trihypot=calc_hypot();
+        this->throwangle=calc_throwangle();
+        this->lesserangle=calc_lesserangle();
+    };
+
+    sewtriangle calc_seamed(const double& seamlength){
+        point hypot_midpoint{this->p1.x-(this->tribase/2),this->p2.y-(this->trileg/2)};
+        //double a_new_tri_hypot=sqrt(pow(this->trihypot,2)+pow(seamlength,2));
+        //double aside_new_tri_hypot=pythagorean_theorum(std::make_optional(seamlength),std::make_optional(this->trihypot/2));
+        
+        //I dont know math so I decided to do a ridiculous solution:
+        double apercent=this->throwangle/90;
+        double bpercent=this->lesserangle/90;   //this is the share of the rest of the 90 degrees possible in the triangle. Ie, A is 66.66, and B is 33.333. 
+        auto newa=this->p1;
+        auto newb=this->p2;
+        double calculated_distance=pythagorean_theorum();  //this gives the distance
+        while (calculated_distance-seamlength>0.01){
+            newa.y+=(0.01*apercent);
+            newb.x-=
+        };
+
+
+    };
+
     sewtriangle(const point& supp1,const point&supp2,bool supqiseam=false,bool supqshowtriangle=true):
         p1(supp1),p2(supp2),qshowtriangle(supqshowtriangle),tripoint(this->calc_tripoint()),
         tribase(calc_tribase()),trileg(calc_trileg()),trihypot(calc_hypot()),throwangle(calc_throwangle()),lesserangle(calc_lesserangle()){};
 
 };
 
-
+//https://www.omnicalculator.com/math/right-triangle-side-angle
+//https://www.calculatorsoup.com/calculators/geometry-plane/distance-two-points.php
 struct sewobject{
 
     sewtriangle normal;
     std::optional<sewtriangle> seamedver;
+    std::optional<sewtriangle> seamcurvefirstpart;
+    std::optional<sewtriangle> seamcurvesecondpart;
     std::optional<sewtriangle> curvefirstpart;
     std::optional<sewtriangle> curvesecondpart;
 
+
     double seamlength;
 
-    void calc_seam(){};
-    void calc_curve(){};
+    sewtriangle calc_seam_ver(const sewtriangle& suptri){  
+        //both points must 'go outwards' while the angles stay the same. 
+        sewtriangle to_return=suptri;  //40/90,  50/90
+        //to_return.p1
 
+        return to_return;};
+    void calc_curve(){};
+    void apply_seam_ver(){};
 
     bool has_curve(){if (this->seamedver){return true;} else {return false;}};
+
     sewobject(const sewtriangle& supnorm,const double& supseamlength=1.5,bool qisseamed=false)
-        :normal(supnorm),seamedver(),curvefirstpart(),curvesecondpart(),seamlength(supseamlength){if (qisseamed){this->calc_seam();}}
+        :normal(supnorm),seamedver(),curvefirstpart(),curvesecondpart(),seamlength(supseamlength){if (qisseamed){this->apply_seam_ver();}}
 
 
 
